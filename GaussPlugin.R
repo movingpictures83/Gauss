c5d67e13d82954cc3209aa7e84ec13469c0ad34d
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2023-12-04 17:14:13 -0500

    Initial

---
+++ GaussPlugin.R
@@ -0,0 +1,50 @@
+library(AppliedPredictiveModeling)
+transparentTheme(trans = .4)
+library(caret)
+
+dyn.load(paste("RPluMA", .Platform$dynlib.ext, sep=""))
+source("RPluMA.R")
+
+
+input <- function(inputfile) {
+	pfix = prefix()
+  parameters <<- read.table(inputfile, as.is=T);
+  rownames(parameters) <<- parameters[,1];
+   # Need to get the three files
+   csvfile <<- paste(pfix, parameters["csvfile", 2], sep="/")
+
+   training <<- read.csv(csvfile)
+   fitControl <<- readRDS(paste(pfix, parameters["fitControl", 2], sep="/"))
+   myType <<- "Regular"
+  if ("type" %in% rownames(parameters)) {
+  myType  <<- as.integer(parameters["type", 2])
+  }
+
+
+}
+
+run <- function() {}
+
+output <- function(outputfile) {
+set.seed(825)
+if (myType == "poly") {
+result <- train(Class ~ ., data = training, 
+                 method = "gaussprPoly", 
+                 trControl = fitControl)
+
+}
+else if (myType == "radial") {
+result <- train(Class ~ ., data = training, 
+                 method = "gaussprRadial", 
+                 trControl = fitControl)
+
+}
+else {
+result <- train(Class ~ ., data = training, 
+#Plugin GaussPoly inputfile Parameters/parameters.train.txt outputfile train/AdaBag.rds
+                 method = "gaussprLinear", 
+                 trControl = fitControl)
+}
+print(result)
+saveRDS(result, outputfile)
+}
